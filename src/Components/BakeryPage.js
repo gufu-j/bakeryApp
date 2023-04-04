6df807cc30529205dcf3c2bdd82e12f530f54a14
gufu-j <112182396+gufu-j@users.noreply.github.com>
2023-04-04 08:54:09 -0400

    Remove comments and console.log() s

--- src/Components/BakeryPage.js
+++ src/Components/BakeryPage.js
@@ -6,18 +6,15 @@ import CakeForm from './CakeForm';
 
 
 function BakeryPage({handleCakeInCart}){
-
-    // Governing State for cake    
+    
     const [cake, setCake] = useState([])
 
-    // Use effect
     useEffect(() => {
       fetch("http://localhost:3000/breads")
         .then((r) => r.json())
         .then((items) => setCake(items));
     }, []);
-
-    // POST a new cake 
+    
     function handleNewCake(newCake){
         setCake([...cake, newCake])
 
@@ -25,14 +22,12 @@ function BakeryPage({handleCakeInCart}){
 
     const [searchCake, setSearchCake] = useState("")
 
-    // Filter Section
     let filteredCakes = cake.filter((el) =>{
         return(
         el.name.toLowerCase().includes(searchCake.toLowerCase())
             )
         })
-
-    //Last Section of This Project PATCH
+    
     function handleUpdatedCake(cakeUpdated){
         const cakesInShopCart = cake.map((el)=> {
             if(el.id === cakeUpdated.id) {
