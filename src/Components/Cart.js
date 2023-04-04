a07b9e9eee004e0aa6efc1e5b43e5d61ae2e85f7
gufu-j <112182396+gufu-j@users.noreply.github.com>
2023-04-04 06:52:50 -0400

    New Cart  Component
    Displays rectangle for each when list of elements appear on this route, before we had it Car.js component, it changes to Cart.js component, added background picture

---
+++ src/Components/Cart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Cart({shopping}){
+
+
+    const myStyle={
+        backgroundImage: 
+        "url('https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/322412816_872757373965464_5501469181507216635_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=19026a&_nc_ohc=9GT9sGDNJvYAX8K5im9&_nc_ht=scontent-lga3-2.xx&oh=00_AfCfHYP5ywiDf7av9jJaXP8y2Udlyb38Up_6eT-VL6gvWw&oe=642C1B89')",
+        height:'100vh',
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        color: "white"
+    };
+
+
+    let shoppingList = shopping.map(el=>{
+        return (
+        <div key={el.id} className='rectangle'>
+        <ul className='list'>{el.name}</ul>
+        </div>
+
+        )
+    })
+
+   // console.log(shoppingList)
+
+
+    return(
+        <center>
+        <div style={myStyle}>
+        <h4> List of cakes you have ordered </h4>
+        {shoppingList}
+        </div>
+        </center>
+    )
+}
+
+export default Cart
