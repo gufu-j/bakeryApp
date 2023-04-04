f8442de84c29feb0e9de6e87564b73f291b61684
gufu-j <112182396+gufu-j@users.noreply.github.com>
2023-04-04 06:43:24 -0400

    route completely updated
    Add background, remove function as prop, center output to the user, added rectangle in the background for each element of list

--- src/Components/Car.js
+++ src/Components/Car.js
@@ -1,26 +1,39 @@
 import React from 'react';
 
-function Car({shopping, handleDelete}){
+function Cart({shopping}){
 
-    
+ 
+    const myStyle={
+        backgroundImage: 
+        "url('https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/322412816_872757373965464_5501469181507216635_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=19026a&_nc_ohc=9GT9sGDNJvYAX8K5im9&_nc_ht=scontent-lga3-2.xx&oh=00_AfCfHYP5ywiDf7av9jJaXP8y2Udlyb38Up_6eT-VL6gvWw&oe=642C1B89')",
+        height:'100vh',
+        marginTop:'-70px',
+        fontSize:'50px',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        color: "white"
+    };
+
 
     let shoppingList = shopping.map(el=>{
-        if(el.isOrdered === false ){
+        return (
+        <div key={el.id} className='rectangle'>
+        <ul className='list'>{el.name}</ul>
+        </div>
 
-        return <li key={el.id}>{el.name}</li>
-    }
-    
+        )
     })
 
-    console.log(shoppingList)
+   // console.log(shoppingList)
 
 
     return(
-        
-        <div>
-        <h4>Your Shopping Car</h4>
+        <center>
+        <div style={myStyle}>
+        <h4> List of cakes you have ordered </h4>
         {shoppingList}
         </div>
+        </center>
     )
 }
 
